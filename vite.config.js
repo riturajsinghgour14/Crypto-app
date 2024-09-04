0ca05cad5db6e9d34b4abb765ee0225d3f71448a
riturajsinghgour14 <148074753+riturajsinghgour14@users.noreply.github.com>
2024-09-03 17:03:54 -0800

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server : {
-    proxy : {
-      "/api" : {
-        target : 'https://authenticationeskills.vercel.app',
-        changeOrigin : true,
-        secure : false,
-      },
-    },
-  },
+  // server : {
+  //   proxy : {
+  //     "/api" : {
+  //       target : 'https://authenticationeskills.vercel.app',
+  //       changeOrigin : true,
+  //       secure : false,
+  //     },
+  //   },
+  // },
    
 });
