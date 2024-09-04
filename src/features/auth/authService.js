10b34fb75a672f95255ecf61a93c75cb1a96bc33
riturajsinghgour14 <148074753+riturajsinghgour14@users.noreply.github.com>
2024-09-03 16:47:17 -0800

    vite.config.js

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = "/api/user/"; 
+const API_URL = "/api/user"; 
 
 const register = async (formData) => {
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,10 @@ import './index.css'
 import { Provider } from 'react-redux'
 import store from './features/store.js'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <Provider store={store}>
+   <Provider store={store} >
      <App />
    </Provider>
   </React.StrictMode>,
